574d59824e6290c7219065c716b376d1cfe36958
zchumager <zchumager@hotmail.com>
2024-08-13 20:09:40 -0700

    Adding raw string for cronjob instructions

--- utils/crud.py
+++ utils/crud.py
@@ -14,6 +14,7 @@ def get_registered_device(partial_mac):
 
 
 def get_active_devices_by_timestamp():
+    # get all the devices which timestamp is greater than now
     devices = session.query(Device).filter(Device.expiration_timestamp >= datetime.now()).all()
     return devices
 
